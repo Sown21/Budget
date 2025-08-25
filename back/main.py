d7fc775274793a695a8015314ac53d3f1b618b2f
Sown21 <sown57@gmail.com>
2025-08-25 08:34:24 +0000

    fix: add CORS to backend

--- back/main.py
+++ back/main.py
@@ -4,6 +4,7 @@ from core.database import engine, Base
 from api.v1.api import api_router
 from models.spent import Spent
 from core.seed_categories import create_defaut_categories
+from fastapi.middleware.cors import CORSMiddleware
 
 # Avant Base.metadata.create_all(bind=engine)
 print("Tables à créer:", list(Base.metadata.tables.keys()))
@@ -17,6 +18,14 @@ Base.metadata.create_all(bind=engine)
 create_defaut_categories()
 
 app = FastAPI(title=settings.app_name, version="1.0.0")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["http://localhost:5173"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # Inclure les routes
 app.include_router(api_router, prefix="/api/v1")
