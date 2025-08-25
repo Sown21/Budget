6d4b7f56e38dedc4d146574d31b5c5adee135275
Sown21 <sown57@gmail.com>
2025-08-25 12:24:01 +0000

    feat: add Table component

--- front/src/components/Table.jsx
+++ front/src/components/Table.jsx
@@ -1,27 +1,29 @@
 const Table = ({ data }) => {
     return (
-        <table className="border border-gray-300">
+        <div className="rounded-lg shadow overflow-hidden mx-8 my-8">
+            <table className="min-w-full border border-gray-300">
                 <thead>
-                    <tr>
-                        <th>Nom</th>
-                        <th>Montant</th>
-                        <th>Description</th>
-                        <th>Catégorie</th>
-                        <th>Date</th>
+                    <tr className="border border-gray-800 bg-gray-800 text-white">
+                        <th className="p-2">Nom</th>
+                        <th className="p-2">Montant</th>
+                        <th className="p-2">Description</th>
+                        <th className="p-2">Catégorie</th>
+                        <th className="p-2">Date</th>
                     </tr>
                 </thead>
                 <tbody>
-                    {data.map((spent) => (
-                        <tr key={spent.id}>
-                            <td>{spent.name}</td>
-                            <td>{spent.amount}</td>
-                            <td>{spent.description}</td>
-                            <td>{spent.category}</td>
-                            <td>{spent.date}</td>
+                    {data.map((spent, idx) => (
+                        <tr key={spent.id} className={idx % 2 === 0 ? "bg-gray-100" : "bg-white"}>
+                            <td className="p-2 border">{spent.name}</td>
+                            <td className="p-2 border">{spent.amount}</td>
+                            <td className="p-2 border">{spent.description}</td>
+                            <td className="p-2 border">{spent.category_name}</td>
+                            <td className="p-2 border">{spent.date}</td>
                         </tr>
                     ))}
                 </tbody>
-        </table> 
+            </table> 
+        </div>
     )
 };
 
