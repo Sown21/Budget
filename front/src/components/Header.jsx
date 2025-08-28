d993db1401db1a8d2ebde7ee7a8632bdb83e876b
Sown21 <sown57@gmail.com>
2025-08-28 08:56:47 +0000

    fix: update Header and add link to dashboard

--- front/src/components/Header.jsx
+++ front/src/components/Header.jsx
@@ -1,7 +1,16 @@
+import { FaMoneyBillTrendUp } from "react-icons/fa6";
+
 const Header = () => {
     return (
         <header className="header_budget shadow_blue">
-
+            <div className="flex mx-auto justify-between">
+                <a className="">
+                    <FaMoneyBillTrendUp />
+                </a>
+                <a className="cursor-pointer text-blue-500 font-semibold hover:scale-105" href="/dashboard">
+                    <span>Dashboard</span>
+                </a>
+            </div>
         </header>
     )
 }
