c0383fed6d0d15c25df08992885d64d4dc06ba81
Sown21 <sown57@gmail.com>
2025-08-29 11:19:27 +0000

    style: update logo style

--- front/src/components/Header.jsx
+++ front/src/components/Header.jsx
@@ -3,9 +3,10 @@ import { FaMoneyBillTrendUp } from "react-icons/fa6";
 const Header = () => {
     return (
         <header className="header_budget shadow_blue">
-            <div className="flex mx-auto justify-between">
-                <a className="">
-                    <FaMoneyBillTrendUp />
+            <div className="flex mx-auto justify-between items-center">
+                <a className="flex gap-4 items-end cursor-pointer" href="/">
+                    <FaMoneyBillTrendUp className="text-3xl text-blue-500" />
+                    <h3 className="font-semibold text-lg">Budget</h3>
                 </a>
                 <div className="flex gap-6">
                     <a className="cursor-pointer text-blue-500 font-semibold hover:scale-105" href="/saisis">
