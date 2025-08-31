57f33500d6cfc1cf9d3fdf592f883eeceb04b216
Sown21 <sown57@gmail.com>
2025-08-31 16:11:35 +0200

    fix: update api timeout

--- front/src/api/axios.js
+++ front/src/api/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const instance = axios.create({
     baseURL: "http://192.168.2.100:32023/api/v1",
-    timeout: 1000,
+    timeout: 5000,
     headers: {
         "Content-Type": "application/json",
     },
