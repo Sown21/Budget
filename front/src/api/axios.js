73e17a1df72d8ea0d833d153c7c851675612b495
Sown21 <sown57@gmail.com>
2025-08-31 16:05:31 +0200

    fix: back port

--- front/src/api/axios.js
+++ front/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-    baseURL: "http://192.168.2.100:38022/api/v1",
+    baseURL: "http://192.168.2.100:32023/api/v1",
     timeout: 1000,
     headers: {
         "Content-Type": "application/json",
