54591605275f79268d591950828a117424cf96ed
Sown21 <sown57@gmail.com>
2025-09-02 06:02:53 +0000

    style: refont UI, remove Header for SideBar

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -3,17 +3,22 @@ import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom"
 import Saisis from "./pages/Saisis"
 import Header from './components/Header'
 import Dashboard from "./pages/Dashboard"
+import SideBar from './components/SideBar'
 
 function App() {
 
   return (
     <BrowserRouter>
-      <Header />
-      <Routes>
-        <Route path="/" element={<Navigate to="/dashboard" replace />} />
-        <Route path ="/saisis" element={<Saisis />} />
-        <Route path ="/dashboard" element={<Dashboard />} />
-      </Routes>
+      <div className="flex min-h-screen">
+        <SideBar />
+        <div className="flex-1">
+          <Routes>
+            <Route path="/" element={<Navigate to="/dashboard" replace />} />
+            <Route path="/saisis" element={<Saisis />} />
+            <Route path="/dashboard" element={<Dashboard />} />
+          </Routes>
+        </div>
+      </div>
     </BrowserRouter>
   )
 }
---
+++ front/src/components/SideBar.jsx
@@ -0,0 +1,45 @@
+import { FaMoneyBillTrendUp } from "react-icons/fa6";
+import { NavLink } from "react-router-dom";
+
+const SideBar = () => {
+    return (
+        <div className="flex min-h-screen">
+            {/* Sidebar */}
+            <aside className="bg-white w-64 p-6 shadow-lg flex flex-col rounded my-10 border border-gray-200">
+                <div className="font-bold text-xl mb-8 flex gap-4 justify-center items-end">
+                    <FaMoneyBillTrendUp className="text-3xl text-blue-500" />
+                    <h2>Budget</h2>
+                </div>
+                <div className="border border-gray-200 my-4"></div>
+                <nav className="flex-1">
+                <ul className="space-y-2">
+                    <li>
+                        <NavLink
+                            to="/dashboard"
+                            className={({ isActive }) =>
+                                "sidebar-link" + (isActive ? " bg-blue-100 text-blue-600 font-semibold" : "")
+                            }
+                        >
+                            Dashboard
+                        </NavLink>
+                    </li>
+                    <li>
+                        <NavLink
+                            to="/saisis"
+                            className={({ isActive }) =>
+                                "sidebar-link" + (isActive ? " bg-blue-100 text-blue-600 font-semibold" : "")
+                            }
+                        >
+                            Saisis
+                        </NavLink>
+                    </li>
+
+                </ul>
+                </nav>
+            </aside>
+
+        </div>
+    )
+}
+
+export default SideBar;
