c8f6f2f253378d9dedb75a67c738f25f2d86bfbc
Sown21 <sown57@gmail.com>
2025-09-05 12:57:04 +0000

    feat: add users front api)

---
+++ front/src/api/users.js
@@ -0,0 +1,17 @@
+import instance from './axios'
+import axios from './axios'
+
+export const getUsers = async () => {
+    const response = await instance.get("/users/")
+    return response.data
+}
+
+export const createUser = async (userData) => {
+    const response = await instance.post("/users/", userData)
+    return response.data
+}
+
+export const deleteUser = async (userId) => {
+    const response = await instance.delete(`/users/${userId}`)
+    return response.data
+}
